a2968bc9ccce5dab773974db5d1586b99638ec91
CornetS28 <corsam28@gmail.com>
2019-12-10 16:02:36 -0500

    Eslint errors fixed

--- src/containers/BooksList.js
+++ src/containers/BooksList.js
@@ -1,17 +1,17 @@
 import React from "react";
 import { connect } from "react-redux";
-// import PropTypes from "prop-types";
 import Book from "../components/Book";
-import CategoryFilter from '../components/CategoryFilter';
-import { setCategoryFilter } from '../actions';
+import CategoryFilter from "../components/CategoryFilter";
+import { setCategoryFilter } from "../actions";
 
-
-const BookList = (props) => {
-  const { books } = props
-  const handleBookByFilterCategotyChange = props.Filterchanged
+const BookList = props => {
+  const { books } = props;
+  const handleBookByFilterCategotyChange = props.Filterchanged;
   return (
     <div>
-      <CategoryFilter handleBookByFilterCategotyChange={handleBookByFilterCategotyChange} />
+      <CategoryFilter
+        handleBookByFilterCategotyChange={handleBookByFilterCategotyChange}
+      />
       <table>
         <thead>
           <tr>
@@ -20,31 +20,28 @@ const BookList = (props) => {
           </tr>
         </thead>
         <tbody>
-          {books.map((book, bookID) =>
-            <Book
-              key={bookID}
-              book={book}
-            />
-          )}
+          {books.map((book, bookID) => (
+            <Book key={bookID} book={book} />
+          ))}
         </tbody>
       </table>
     </div>
   );
-}
-const mapStateToProps = (state) => {
+};
+const mapStateToProps = state => {
   let { books, filter } = state;
-  if (filter !== '') {
-    books = books.filter((book) => filter === book.category)
+  if (filter !== "") {
+    books = books.filter(book => filter === book.category);
   }
   return {
     books
-  }
-}
+  };
+};
 
-const mapDispatchToProps = (dispatch) => {
+const mapDispatchToProps = dispatch => {
   return {
-    Filterchanged: (filter) => dispatch(setCategoryFilter(filter))
-  }
-}
+    Filterchanged: filter => dispatch(setCategoryFilter(filter))
+  };
+};
 
-export default connect(mapStateToProps, mapDispatchToProps)(BookList)
+export default connect(mapStateToProps, mapDispatchToProps)(BookList);
