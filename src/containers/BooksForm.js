4fdc7b148e65a51c341871cd34a1481c9b57d916
CornetS28 <corsam28@gmail.com>
2019-12-12 17:21:33 -0500

    css styling

--- src/containers/BooksForm.js
+++ src/containers/BooksForm.js
@@ -1,9 +1,74 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { createBook } from '../actions';
-import categories from './constantCategories';
+import React from "react";
+import { connect } from "react-redux";
+import { createBook } from "../actions";
+import categories from "./constantCategories";
 
+const insertNewBook = {
+  width: "174px",
+  height: "24px",
+  fontFamily: "Montserrat",
+  fontSize: "20px",
+  fontWeight: "bold",
+  fontStretch: "normal",
+  fontStyle: "normal",
+  lineHeight: "normal",
+  letterSpacing: "-0.18px",
+  color: "#888888",
+  marginLeft: "17%",
+  marginBottom: "-2%",
+  marginTop: "3%"
+};
 
+const inputStyle = {
+  width: "664px",
+  height: "45px",
+  borderRadius: "4px",
+  border: "solid 1px #e8e8e8",
+  backgroundColor: "#ffffff"
+};
+
+const categorySelection = {
+  fontFamily: "Montserrat",
+  fontSize: "16px",
+  width: "162px",
+  height: "19px",
+  borderRadius: "4px",
+  boxShadow: "0 0 60px 0 var(--black - 5)",
+  backgroundColor: "#fafafa",
+  marginTop: "4%"
+};
+const categoryWrapper = {
+  width: "25%",
+  height: "45px",
+  borderRadius: "4px",
+  border: "solid 1px #e8e8e8",
+  backgroundColor: "#ffffff",
+  textAlign: "center",
+  fontSize: "15px"
+};
+
+const formWrapper = {
+  width: "70%",
+  margin: "0 auto",
+  display: "flex",
+  justifyContent: "space-around",
+  padding: " 50px 0",
+  borderTop: "solid 1px #e8e8e8"
+};
+
+const bookBtnWrapper = {
+  width: "184px",
+  height: "45px",
+  borderRadius: "3px",
+  backgroundColor: "#0290ff",
+  color: "#ffffff",
+  fontFamily: "RobotoSlab",
+  fontWeight: "bold",
+  fontStretch: "normal",
+  letterSpacing: "0.5px",
+  textAlign: "center",
+  fontSize: "13px"
+};
 const ChooseACategory = ({ value, handleChange }) => {
   const options = categories.map(cat => (
     <option key={cat} value={cat}>
@@ -13,6 +78,7 @@ const ChooseACategory = ({ value, handleChange }) => {
 
   return (
     <select
+      style={categorySelection}
       value={value}
       onChange={handleChange}
       name="category"
@@ -31,9 +97,9 @@ class BooksForm extends React.Component {
     super(props);
     this.state = {
       book: {
-        title: '',
-        category: categories[0],
-      },
+        title: "",
+        category: categories[0]
+      }
     };
 
     this.handleChange = this.handleChange.bind(this);
@@ -45,54 +111,68 @@ class BooksForm extends React.Component {
     this.setState({
       book: {
         ...book,
-        [name]: value,
-      },
+        [name]: value
+      }
     });
   }
 
   handleBookSubmit(event) {
     const {
       state: { book },
-      props: { createBook },
+      props: { createBook }
     } = this;
 
     event.preventDefault();
     createBook(book);
     this.setState({
       book: {
-        title: '',
-        category: categories[0],
-      },
+        title: "",
+        category: categories[0]
+      }
     });
   }
 
   render() {
     const {
-      book: { title, category },
+      book: { title, category }
     } = this.state;
 
     return (
-      <form action="">
-        <h2>Insert new book</h2>
-        <input
-          placeholder="Enter Book Title"
-          type="text"
-          name="title"
-          id="booo--title"
-          minLength="4"
-          value={title}
-          onChange={this.handleChange}
-          required
-        />
+      <div>
+        <span>
+          <h2 style={insertNewBook}>Insert new book</h2>
+        </span>
 
-        <div className="">
-          <ChooseACategory handleChange={this.handleChange} value={category} />
+        <div style={formWrapper}>
+          <input
+            style={inputStyle}
+            placeholder="Enter Book Title"
+            type="text"
+            name="title"
+            id="booo--title"
+            minLength="4"
+            value={title}
+            onChange={this.handleChange}
+            required
+          />
+
+          <div style={categoryWrapper}>
+            <ChooseACategory
+              handleChange={this.handleChange}
+              value={category}
+            />
+          </div>
+
+          <button
+            style={bookBtnWrapper}
+            type="button"
+            id="add-a-book"
+            onClick={this.handleBookSubmit}
+          >
+            Add This Book
+          </button>
         </div>
-
-        <button type="button" id="add-a-book" onClick={this.handleBookSubmit}>
-          Add This Book
-        </button>
-      </form>
+      </div>
     );
   }
 }
--- src/containers/BooksList.js
+++ src/containers/BooksList.js
@@ -1,31 +1,51 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import Book from '../components/Book';
-import CategoryFilter from '../components/CategoryFilter';
-import { setCategoryFilter } from '../actions';
+import React from "react";
+import { connect } from "react-redux";
+import Book from "../components/Book";
+import CategoryFilter from "../components/CategoryFilter";
+import { setCategoryFilter } from "../actions";
 
 const BookList = props => {
+  const navbar = {
+    width: "100%",
+    height: "95px",
+    backgroundColor: "#ffffff",
+    display: "flex",
+    justifyContent: "space - around",
+    alignItems: "center",
+    border: "solid 1px #e8e8e8"
+  };
+
+  const pagesLogo = {
+    width: "240px",
+    height: "37px",
+    fontFamily: "Montserrat",
+    fontSize: "30px",
+    fontWeight: "800",
+    fontStyle: "normal",
+    fontStretch: "normal",
+    lineHeight: "normal",
+    letterSpacing: "normal",
+    color: "#0290ff",
+    marginLeft: "16%"
+  };
+
   const { books } = props;
   const handleBookByFilterCategotyChange = props.Filterchanged;
   return (
-    <div>
-      <CategoryFilter
-        handleBookByFilterCategotyChange={handleBookByFilterCategotyChange}
-      />
-      <table>
-        <thead>
-          <tr>
-            <th>title</th>
-            <th>category</th>
-          </tr>
-        </thead>
-        <tbody>
-          {books.map((book, bookID) => (
-            <Book key={bookID} book={book} />
-          ))}
-        </tbody>
-      </table>
-    </div>
+    <main>
+      <nav style={navbar}>
+        <h1 style={pagesLogo}>Bookstore CMS</h1>
+        <CategoryFilter
+          handleBookByFilterCategotyChange={handleBookByFilterCategotyChange}
+        />
+      </nav>
+      <div />
+      {books.map((book, bookID) => (
+        <ul>
+          <Book key={bookID} book={book} />
+        </ul>
+      ))}
+    </main>
   );
 };
 
@@ -33,13 +53,13 @@ const mapStateToProps = state => {
   const { books, filter } = state;
   return {
     books:
-      filter === '' ? books : books.filter(book => filter === book.category),
+      filter === "" ? books : books.filter(book => filter === book.category)
   };
 };
 
 const mapDispatchToProps = dispatch => {
   return {
-    Filterchanged: filter => dispatch(setCategoryFilter(filter)),
+    Filterchanged: filter => dispatch(setCategoryFilter(filter))
   };
 };
 
